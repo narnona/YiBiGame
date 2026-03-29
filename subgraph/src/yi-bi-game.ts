e2b22c4878bda688215d2752e6cd4d17798c1ed4
narnona <tolele666@gmail.com>
2026-03-29 10:50:18 +0800

    Initialize subgraph

--- subgraph/src/yi-bi-game.ts
+++ subgraph/src/yi-bi-game.ts
@@ -1,64 +1,38 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts"
 import {
-  YiBiGame,
-  LevelCreated,
-  LevelSolved
+  LevelCreated as LevelCreatedEvent,
+  LevelSolved as LevelSolvedEvent
 } from "../generated/YiBiGame/YiBiGame"
-import { ExampleEntity } from "../generated/schema"
+import { LevelCreated, LevelSolved } from "../generated/schema"
 
-export function handleLevelCreated(event: LevelCreated): void {
-  // Entities can be loaded from the store using an ID; this ID
-  // needs to be unique across all entities of the same type
-  const id = event.transaction.hash.concat(
-    Bytes.fromByteArray(Bytes.fromBigInt(event.logIndex))
+export function handleLevelCreated(event: LevelCreatedEvent): void {
+  let entity = new LevelCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  let entity = ExampleEntity.load(id)
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(id)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.levelId = event.params.levelId
   entity.creator = event.params.creator
+  entity.name = event.params.name
+  entity.size = event.params.size
+  entity.hintsCount = event.params.hintsCount
 
-  // Entities can be written to the store with `.save()`
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.createLevel(...)
-  // - contract.getLevel(...)
-  // - contract.getLevels(...)
-  // - contract.getLevelsCount(...)
-  // - contract.hasCompleted(...)
-  // - contract.hasPlayerCompleted(...)
-  // - contract.levelExists(...)
-  // - contract.levelHashes(...)
-  // - contract.levels(...)
-  // - contract.nextLevelId(...)
-  // - contract.verifySolution(...)
 }
 
-export function handleLevelSolved(event: LevelSolved): void {}
+export function handleLevelSolved(event: LevelSolvedEvent): void {
+  let entity = new LevelSolved(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.levelId = event.params.levelId
+  entity.solver = event.params.solver
+  entity.pathLength = event.params.pathLength
+  entity.isFirstCompletion = event.params.isFirstCompletion
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
--- subgraph/tests/yi-bi-game.test.ts
+++ subgraph/tests/yi-bi-game.test.ts
@@ -7,8 +7,8 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { BigInt, Address } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { LevelCreated } from "../generated/YiBiGame/YiBiGame"
+import { LevelCreated } from "../generated/schema"
+import { LevelCreated as LevelCreatedEvent } from "../generated/YiBiGame/YiBiGame"
 import { handleLevelCreated } from "../src/yi-bi-game"
 import { createLevelCreatedEvent } from "./yi-bi-game-utils"
 
@@ -41,37 +41,37 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("LevelCreated created and stored", () => {
+    assert.entityCount("LevelCreated", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "LevelCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "levelId",
       "234"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "LevelCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "creator",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "LevelCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "name",
       "Example string value"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "LevelCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "size",
       "123"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "LevelCreated",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "hintsCount",
       "234"
     )
